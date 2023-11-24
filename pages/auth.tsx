a2eb4e3cfed0c8ce5f4a96868035167d8a2cad16
Danil Deineka <tripledpadik@gmail.com>
2023-11-24 22:48:13 +0200

    Add google + git hub auth

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,8 +1,29 @@
-import Input from "@/components/Input";
-import { useCallback, useState } from "react";
 import axios from "axios";
-import { signIn } from "next-auth/react";
+import { useCallback, useState } from "react";
+import { NextPageContext } from "next";
+import { getSession, signIn } from "next-auth/react";
 import { useRouter } from "next/router";
+import { FcGoogle } from "react-icons/fc";
+import { FaGithub } from "react-icons/fa";
+
+import Input from "@/components/Input";
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+
+  if (session) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+}
 
 const Auth = () => {
   const router = useRouter();
@@ -28,7 +49,7 @@ const Auth = () => {
         callbackUrl: "/",
       });
 
-      router.push("/");
+      router.push("/profiles");
     } catch (error) {
       console.log(error);
     }
@@ -36,7 +57,11 @@ const Auth = () => {
 
   const register = useCallback(async () => {
     try {
-      await axios.post("/api/register", { email, name, password });
+      await axios.post("/api/register", {
+        email,
+        name,
+        password,
+      });
 
       login();
     } catch (error) {
@@ -48,7 +73,7 @@ const Auth = () => {
     <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
       <div className="bg-black w-full h-full lg:bg-opacity-50">
         <nav className="px-12 py-5">
-          <img src="/images/logo.png" alt="logo" className="h-12" />
+          <img src="/images/logo.png" className="h-12" alt="Logo" />
         </nav>
         <div className="flex justify-center">
           <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
@@ -58,26 +83,26 @@ const Auth = () => {
             <div className="flex flex-col gap-4">
               {variant === "register" && (
                 <Input
-                  label="Username"
-                  onChange={(event: any) => setName(event.target.value)}
                   id="name"
+                  type="text"
+                  label="Username"
                   value={name}
+                  onChange={(e: any) => setName(e.target.value)}
                 />
               )}
-
               <Input
-                label="Email"
-                onChange={(event: any) => setEmail(event.target.value)}
                 id="email"
                 type="email"
+                label="Email address or phone number"
                 value={email}
+                onChange={(e: any) => setEmail(e.target.value)}
               />
               <Input
-                label="Password"
-                onChange={(event: any) => setPassword(event.target.value)}
-                id="password"
                 type="password"
+                id="password"
+                label="Password"
                 value={password}
+                onChange={(e: any) => setPassword(e.target.value)}
               />
             </div>
             <button
@@ -86,6 +111,20 @@ const Auth = () => {
             >
               {variant === "login" ? "Login" : "Sign up"}
             </button>
+            <div className="flex flex-row items-center gap-4 mt-8 justify-center">
+              <div
+                onClick={() => signIn("google", { callbackUrl: "/profiles" })}
+                className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition"
+              >
+                <FcGoogle size={32} />
+              </div>
+              <div
+                onClick={() => signIn("github", { callbackUrl: "/profiles" })}
+                className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition"
+              >
+                <FaGithub size={32} />
+              </div>
+            </div>
             <p className="text-neutral-500 mt-12">
               {variant === "login"
                 ? "First time using Netflix?"
@@ -96,6 +135,7 @@ const Auth = () => {
               >
                 {variant === "login" ? "Create an account" : "Login"}
               </span>
+              .
             </p>
           </div>
         </div>
