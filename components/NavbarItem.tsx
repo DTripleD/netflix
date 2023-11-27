19bf402f472e28ea2ff32bbb2ab65aece6fd65dc
Danil Deineka <tripledpadik@gmail.com>
2023-11-27 22:50:56 +0200

    Add navbar

---
+++ components/NavbarItem.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+interface NavbatItemProps {
+  label: string;
+}
+
+const NavbarItem: React.FC<NavbatItemProps> = ({ label }) => {
+  return (
+    <div className="text-white cursor-pointer hover:text-gray-300 transition">
+      {label}
+    </div>
+  );
+};
+
+export default NavbarItem;
