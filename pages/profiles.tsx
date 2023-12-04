b8b99038df6623d121950c3bfec48b9e6fa20f33
Danil Deineka <tripledpadik@gmail.com>
2023-12-04 15:38:38 +0200

    bug fix auth redirect

--- pages/profiles.tsx
+++ pages/profiles.tsx
@@ -17,20 +17,21 @@ interface UserCardProps {
 }
 
 export async function getServerSideProps(context: NextPageContext) {
-  const session = await getSession(context);
-
-  if (!session) {
-    return {
-      redirect: {
-        destination: "/auth",
-        permanent: false,
-      },
-    };
+  try {
+    const session = await getSession(context);
+    if (!session) {
+      return {
+        redirect: {
+          destination: "/auth",
+          permanent: false,
+        },
+      };
+    }
+    return { props: {} };
+  } catch (error) {
+    console.error("Error getting session:", error);
+    return { props: {} };
   }
-
-  return {
-    props: {},
-  };
 }
 
 const UserCard: React.FC<UserCardProps> = ({ name }) => {
