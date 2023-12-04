56774155266b40c08cc1c9a12e578c708ebe2164
Danil Deineka <tripledpadik@gmail.com>
2023-12-04 13:46:13 +0200

    Bug fix ts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,12 @@ import type { AppProps } from "next/app";
 
 import "../styles/globals.css";
 
+type ComponentWithSession = React.ComponentType<AppProps & { session: any }>;
+
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
-}: AppProps) {
+}: AppProps & { Component: ComponentWithSession }) {
   return (
     <SessionProvider session={session}>
       <Component {...pageProps} />
